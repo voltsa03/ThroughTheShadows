59413feb7f76ab3677a5a8c48a6d0f2e48a64f8b
Muji <rasmusiam@gmail.com>
2023-03-15 20:33:08 +0000

    movement script
    added jumping
    wall falling
    accelaration

--- Assets/Scripts/PlayerMovment.cs
+++ Assets/Scripts/PlayerMovment.cs
@@ -4,50 +4,133 @@ using UnityEngine;
 
 public class PlayerMovment : MonoBehaviour
 {
+ 
+    [SerializeField]
+    private int maxSpeed, speedMultipler, jumpMultiplyer, fallSpeed;
 
+    [SerializeField]
+    private float speedX;
+
+    [SerializeField]
+    private float stoppingSpeed;
+
+
+
+    private float rayDistance, sideRayDistance;
+
+    private bool grounded = false, notFalling = true;
     private Rigidbody rb;
 
-    [SerializeField]
-    private int rayLayer = 3;
-    [SerializeField]
-    private int maxSpeed, speedMultipler, curspeedX;
-    
+    private Vector2 velocitySpeed;
 
+    int groundLayerMask = 1 << 3;
+    
 
 
     private void Awake()
     {
         rb = GetComponent<Rigidbody>();
+       
+    }
+    private void FixedUpdate()
+    {
+        rb.velocity = new Vector2(velocitySpeed.x, rb.velocity.y);
     }
 
     void Update()
     {
+        //Vector3 down = transform.TransformDirection(Vector3.down) / 2;
+        //Debug.DrawRay(transform.position, down, Color.red);
+        movement();
+
         jumping();
 
-        movement();
+        rayCasting();
+
+        debuging();
     }
-
-
 
     void movement()
     {
-        Vector3 horizontal = transform.TransformDirection(Vector3.right);
+        Vector2 horizontal = transform.TransformDirection(Vector2.right);
+      
 
-        if (Input.GetKey(KeyCode.D) && (curspeedX < maxSpeed))
-            curspeedX += speedMultipler;
+        if (Input.GetKey(KeyCode.D) && (speedX < maxSpeed) && grounded && notFalling)
+            speedX += speedMultipler;
         
            // curspeedX -= (speedMultipler / 2);
+
+        else if (Input.GetKey(KeyCode.A) && (speedX > -maxSpeed) && grounded && notFalling)
+            speedX -= speedMultipler;
+
+        else 
+            stopping();
+
+        velocitySpeed = horizontal * speedX;
     }
+
     void jumping()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space) && grounded)
         {
-
-            if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.down), 1))// shoots a ray cast bellow the character
-            {
-                Debug.Log("raycast hit");
-            }
+          
+                rb.velocity = new Vector2(rb.velocity.x, jumpMultiplyer);
         }
+    }
+           
+
+    void rayCasting()
+    {
+        RaycastHit hitDown;
+        if (Physics.Raycast(transform.position, Vector3.down, out hitDown, 1, groundLayerMask))// shoots a ray cast bellow the character
+        {
+            rayDistance = hitDown.distance;
+            if (rayDistance < 0.51)//_____________ set character hight__________________________________
+                grounded = true;
+            else
+                grounded = false;
+        }
+      
+
+        RaycastHit hitSide;
+        if (Physics.Raycast(transform.position, Vector3.right, out hitSide, 1) || Physics.Raycast(transform.position, Vector3.left, out hitSide, 1))
+        {
+            sideRayDistance = hitSide.distance;
+            if (sideRayDistance < 0.51 && grounded == false)// _______________set character thickness__________________________
+            {
+                speedX = 0;
+                notFalling = false;
+            }
+            else
+                notFalling = true;
+                
+        }
+    }
+
+    void stopping()
+    {
+        if(speedX != 0 && grounded)
+        {
+            if (speedX > 0)
+                speedX -= stoppingSpeed;
+            else if (speedX < 0)
+                speedX += stoppingSpeed;
+            else
+                speedX = 0;
+        }
+
+        /*if (speedX == 30 && grounded == false)
+        {
+            Debug.Log("falling is progress");
+            rb.velocity = new Vector2(rb.velocity.x, -fallSpeed);
+        }*/
+    }
+      void debuging()
+    {
+        Debug.Log(notFalling + " not falling");
+        
+        Debug.Log(grounded + " grounded");
+  
 
     }
 }
