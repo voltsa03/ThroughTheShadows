94ee1da9f6f11e5d6865cab910b833bcda2b91ed
Muji <rasmusiam@gmail.com>
2023-03-16 03:44:48 +0000

    added scripts

---
+++ Assets/Scripts/experimental scripts/Enemy.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public float speed;
+    public float stoppingDistance;
+    public float retreatdistance;
+
+    private float timeBtwShots;
+    public float startTimeBtwShots;
+
+    public GameObject projectile;
+    public Transform player;
+
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+
+        timeBtwShots = startTimeBtwShots;
+    }
+    void Update()
+    {
+        if (Vector2.Distance(transform.position, player.position) >stoppingDistance){
+            transform.position = Vector2.MoveTowards(transform.position, player.position, speed * Time.deltaTime);
+        } else if (Vector2.Distance(transform.position, player.position) < stoppingDistance && Vector2.Distance(transform.position, player.position) > retreatdistance) {
+            transform.position = this.transform.position;
+        } else if (Vector2.Distance(transform.position, player.position) < retreatdistance){
+            transform.position = Vector2.MoveTowards(transform.position, player.position, -speed * Time.deltaTime);
+        }
+        if (timeBtwShots <=0){
+            Instantiate(projectile, transform.position, Quaternion.identity);
+            timeBtwShots = startTimeBtwShots;
+        } else {
+            timeBtwShots -= Time.deltaTime;
+        }
+    }
+}
